8707e303c15bddeeb59348fe96af90c996a228ff
mswift42 <martin.haesler@gmail.com>
2013-03-17 11:53:57 +0000

    added key-chord.

--- init.el
+++ init.el
@@ -15,6 +15,9 @@
 
 ;; end of elpa setup
 
+;; add key-chords
+(require 'key-chord)
+(key-chord-mode 1)
 
 ;; evil setup:
 
@@ -23,12 +26,15 @@
 (require 'surround)
 (evil-mode 1)
 
+(key-chord-define evil-normal-state-map "kj" 'evil-force-normal-state)
+(key-chord-define evil-visual-state-map "kj" 'evil-change-to-previous-state)
+(key-chord-define evil-insert-state-map "kj" 'evil-normal-state)
+(key-chord-define evil-replace-state-map "kj" 'evil-normal-state)
+
 ;; set theme :
 (load-theme 'gruber-darker t)
 
 
-
-
-
-
+;; add winner-mode
+(winner-mode 1)
 
