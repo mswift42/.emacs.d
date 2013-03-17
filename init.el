ed3456566fa9458ffd954fee6290e50ebe0b9590
mswift42 <martin.haesler@gmail.com>
2013-03-17 11:36:38 +0000

    * elpa/surround-20121022.1257/surround-autoloads.el: add surround

--- init.el
+++ init.el
@@ -1,3 +1,10 @@
+;; disable tool and Menu bar:
+(tool-bar-mode -1)
+(menu-bar-mode -1)
+
+
+;; add repos for elpa
+
 (require 'package)
 
  (setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
@@ -5,6 +12,20 @@
                            ("melpa" . "http://melpa.milkbox.net/packages/")))
 
 (package-initialize)
+
+;; end of elpa setup
+
+
+;; evil setup:
+
+(require 'evil)
+(require 'evil-leader)
+(require 'surround)
+(evil-mode 1)
+
+;; set theme :
+(load-theme 'gruber-darker t)
+
 
 
 
