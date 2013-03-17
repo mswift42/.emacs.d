a90ada18ecd551f87e5ed305608a5595d6b528d0
mswift42 <martin.haesler@gmail.com>
2013-03-17 11:02:47 +0000

    repos added.

---
+++ init.el
@@ -0,0 +1,13 @@
+(require 'package)
+
+ (setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
+                           ("marmalade" . "http://marmalade-repo.org/packages/")
+                           ("melpa" . "http://melpa.milkbox.net/packages/")))
+
+(package-initialize)
+
+
+
+
+
+
