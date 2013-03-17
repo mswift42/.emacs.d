0655c0e6b8a2810daaa3aba34c474b9843313a31
mswift42 <martin.haesler@gmail.com>
2013-03-17 20:02:37 +0000

    deleted helm.

--- elpa/helm-ag-20130316.1610/helm-ag-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "helm-ag" "20130316.1610" "the silver search with helm interface [source: github]" (quote ((helm "1.0"))))
--- elpa/helm-descbinds-20130223.1553/helm-descbinds-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "helm-descbinds" "20130223.1553" "Yet Another `describe-bindings' with `helm'. [source: github]" (quote nil))
