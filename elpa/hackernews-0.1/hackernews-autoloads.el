475a4ce26874c1bf73ce3d6bbcbd9487d2da8143
mswift42 <martin.haesler@gmail.com>
2013-03-18 16:41:57 +0000

    deleted helm, update jedi.

---
+++ elpa/hackernews-0.1/hackernews-autoloads.el
@@ -0,0 +1,28 @@
+;;; hackernews-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads (hackernews) "hackernews" "hackernews.el" (20807
+;;;;;;  16991))
+;;; Generated autoloads from hackernews.el
+
+(autoload 'hackernews "hackernews" "\
+The entry point of our client
+
+\(fn)" t nil)
+
+;;;***
+
+;;;### (autoloads nil nil ("hackernews-pkg.el") (20807 16991 70421))
+
+;;;***
+
+(provide 'hackernews-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; hackernews-autoloads.el ends here
