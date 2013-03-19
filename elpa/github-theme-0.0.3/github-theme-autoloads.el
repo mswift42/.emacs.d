40d269965524eedf9c04e3779a7317a413e9c3c0
mswift42 <martin.haesler@gmail.com>
2013-03-19 17:58:01 +0000

    org-mode changes.

---
+++ elpa/github-theme-0.0.3/github-theme-autoloads.el
@@ -0,0 +1,18 @@
+;;; github-theme-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads nil nil ("github-theme-pkg.el" "github-theme.el")
+;;;;;;  (20808 42703 539667 895000))
+
+;;;***
+
+(provide 'github-theme-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; github-theme-autoloads.el ends here
