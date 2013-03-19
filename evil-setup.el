8cd5adcf8963cf917f59dea7f5f513e2756859cc
mswift42 <martin.haesler@gmail.com>
2013-03-19 17:58:26 +0000

    org-mode

--- evil-setup.el
+++ evil-setup.el
@@ -4,12 +4,14 @@
 
 (require 'evil-leader)
 (require 'surround)
+(setq evil-default-cursor t)
 (evil-mode 1)
 
 (key-chord-define evil-normal-state-map "kj" 'evil-force-normal-state)
 (key-chord-define evil-visual-state-map "kj" 'evil-change-to-previous-state)
 (key-chord-define evil-insert-state-map "kj" 'evil-normal-state)
 (key-chord-define evil-replace-state-map "kj" 'evil-normal-state)
+
 
 ;; evil-leader setup:
 
@@ -18,7 +20,6 @@
   "m" 'magit-status
   "g r" 'golden-ratio
   "t w" 'toggle-window-split
-  "c" 'kill-buffer
   "f" 'find-file
   "b" 'switch-to-buffer
   "h" 'windmove-left
--- init.el
+++ init.el
@@ -146,5 +146,53 @@
 
 ;; change flymake faces to underlines:
 (custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
  '(flymake-errline ((((class color)) (:underline "red"))))
  '(flymake-warnline ((((class color)) (:underline "yellow")))))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes (quote ("967c58175840fcea30b56f2a5a326b232d4939393bed59339d21e46cf4798ecf" default))))
+
+;; Org-mode setup:
+
+(custom-set-variables
+ '(org-agenda-files (quote ("~/todo.org")))
+ '(org-default-notes-file "~/notes.org")
+ '(org-agenda-ndays 7)
+ '(org-deadline-warning-days 14)
+ '(org-agenda-show-all-dates t)
+ '(org-agenda-skip-deadline-if-done t)
+ '(org-agenda-skip-scheduled-if-done t)
+ '(org-agenda-start-on-weekday nil)
+ '(org-reverse-note-order t)
+ '(org-fast-tag-selection-single-key (quote expert))
+ '(org-agenda-custom-commands
+   (quote (("d" todo "DELEGATED" nil)
+       ("c" todo "DONE|DEFERRED|CANCELLED" nil)
+       ("w" todo "WAITING" nil)
+       ("W" agenda "" ((org-agenda-ndays 21)))
+       ("A" agenda ""
+        ((org-agenda-skip-function
+          (lambda nil
+        (org-agenda-skip-entry-if (quote notregexp) "\\=.*\\[#A\\]")))
+         (org-agenda-ndays 1)
+         (org-agenda-overriding-header "Today's Priority #A tasks: ")))
+       ("u" alltodo ""
+        ((org-agenda-skip-function
+          (lambda nil
+        (org-agenda-skip-entry-if (quote scheduled) (quote deadline)
+                      (quote regexp) "\n]+>")))
+         (org-agenda-overriding-header "Unscheduled TODO entries: ")))))))
+
+(setq org-default-notes-file (expand-file-name "~/notes.org"))
+(setq org-capture-templates
+      '(("t" "Todo" entry (file+headline "~/todo.org" "Tasks")
+         "* TODO %?\n  %i\n  %a")))
+
+;; end of org-mode .
