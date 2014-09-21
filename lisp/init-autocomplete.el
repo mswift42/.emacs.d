c003d29ec216555800178d1fce1856e1354b79dc
martin.haesler <martin.haesler@gmail.com>
2014-09-21 20:29:04 +0100

    Add css-autocompletion

--- lisp/init-autocomplete.el
+++ lisp/init-autocomplete.el
@@ -64,6 +64,17 @@
 
 (setq dabbrev-friend-buffer-function 'sanityinc/dabbrev-friend-buffer)
 
+;;; Auto-complete CSS keywords
+(after-load 'auto-complete
+  (dolist (hook '(css-mode-hook sass-mode-hook scss-mode-hook))
+    (add-hook hook 'ac-css-mode-setup)))
+
+;;; Use eldoc for syntax hints
+(require-package 'css-eldoc)
+(autoload 'turn-on-css-eldoc "css-eldoc")
+(add-hook 'css-mode-hook 'turn-on-css-eldoc)
+
+
 
 (provide 'init-autocomplete)
 
