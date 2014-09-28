e2fd4ce38dbcd415b925bd70ea8d8765468ae502
martin.haesler <martin.haesler@gmail.com>
2014-09-28 16:23:48 +0100

    Add paste bindings

--- lisp/init-evil.el
+++ lisp/init-evil.el
@@ -1,5 +1,7 @@
 ;;; Setup Evil
 (require-package 'evil)
+(evil-mode t)
+(require-package 'evil-leader)
 (require-package 'evil-nerd-commenter)
 (require-package 'evil-surround)
 (require-package 'evil-matchit)
@@ -11,10 +13,22 @@
 (setq evil-visual-state-cursor '("orange" box))
 (setq evil-insert-state-cursor '("red" bar))
 (setq evil-replace-state-cursor '("red" bar))
-(setq evil-operator-state-cursor '("red" hollow))
+(setq evil-operator-state-cursor '("red" ))
 
 
 (define-key evil-insert-state-map (kbd "C-g") 'evil-normal-state)
+(global-evil-leader-mode)
+(evil-leader/set-leader ",")
+(evil-leader/set-key "f" 'evil-ace-jump-word-mode)
+(global-evil-surround-mode 1)
+(define-key evil-normal-state-map "p" 'evil-paste-after)
+(define-key evil-normal-state-map "P" 'evil-paste-before)
+;; undo
+(define-key evil-normal-state-map "u" 'undo)
 
-
+(when (fboundp 'undo-tree-undo)
+  (define-key evil-normal-state-map "u" 'undo-tree-undo)
+  (define-key evil-normal-state-map "\C-r" 'undo-tree-redo))
+(define-key evil-normal-state-map "=" 'evil-indent)
 (provide 'init-evil)
+;;; init-evil ends here
