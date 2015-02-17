de8b0e278ae55f04fff3e3dfe5278f02ae75185e
martin.haesler <martin.haesler@gmail.com>
2015-02-17 18:41:45 +0000

    add magit setup

---
+++ init.el
@@ -0,0 +1,41 @@
+;; set custom file
+(setq custom-file "~/.emacs.d/custom.el")
+(load custom-file)
+
+;; package init
+
+(require 'package)
+(setq package-enable-at-startup nil)
+(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
+
+(package-initialize)
+
+;; Bootstrap `use-package'
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+(require 'use-package)
+
+;; load elisp-slime-nav
+(use-package elisp-slime-nav
+  :ensure t
+  :init
+  (dolist (i '(emacs-lisp-mode-hook ielm-mode-hook))
+    (add-hook i 'elisp-slime-nav-mode)))
+
+
+(add-to-list 'load-path (expand-file-name "lisp" user-emacs-directory))
+
+
+(require 'init-appearance)
+
+;; setup magit
+(use-package magit
+  :ensure t
+  :bind (("M-<f12>" . magit-status)))
+
+
+
+
+
