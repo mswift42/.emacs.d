ca47468436b42e4f75aaee8aa51768e93d77ff7e
martin.haesler <martin.haesler@gmail.com>
2015-02-27 14:21:12 +0000

    add init-evil file

---
+++ lisp/init-evil.el
@@ -0,0 +1,54 @@
+;; setup evil mode
+
+(use-package evil
+  :ensure t
+  :init
+  (progn
+    (evil-mode t)
+    (define-key evil-insert-state-map (kbd "C-g") 'evil-normal-state))
+  :config
+  (progn
+    (use-package evil-leader
+      :ensure t
+      :init
+      (progn
+        (global-evil-leader-mode)
+        (evil-leader/set-leader "<SPC>")
+        (evil-leader/set-key "f" 'evil-ace-jump-word-mode)
+        (evil-leader/set-key "c" 'compile)
+        (evil-leader/set-key "m" 'magit-status)
+        (evil-leader/set-key "h" 'helm-mini)
+        (evil-leader/set-key "of" 'helm-find-files)
+        (evil-leader/set-key "vb" 'ido-switch-buffer)
+        (evil-leader/set-key "sc" 'org-capture)
+        (evil-leader/set-key "k" 'ido-kill-buffer)))
+    (use-package evil-surround
+      :ensure t
+      :defer t
+      :idle
+      (progn
+        (global-evil-surround-mode)))
+    (defvar evil-emacs-state-cursor)
+    (defvar evil-normal-state-cursor)
+    (defvar evil-visual-state-cursor)
+    (defvar evil-insert-state-cursor)
+    (defvar evil-replace-state-cursor)
+    (defvar evil-operator-state-cursor)
+    (defvar evil-normal-state-map)
+    (defvar evil-insert-state-map)
+
+    (setq evil-emacs-state-cursor '("red" box))
+    (setq evil-normal-state-cursor '("green" box))
+    (setq evil-visual-state-cursor '("orange" box))
+    (setq evil-insert-state-cursor '("red" bar))
+    (setq evil-replace-state-cursor '("red" bar))
+    (setq evil-operator-state-cursor '("red" ))
+    (define-key evil-normal-state-map "p" 'evil-paste-after)
+    (define-key evil-normal-state-map "P" 'evil-paste-before)
+    (define-key evil-normal-state-map (kbd "C-]") (kbd "\\ M-."))
+    ))
+
+(provide 'init-evil)
+;;; init-evil ends here
+
+
